7ca7f502a92071255d30d170270859da54d52fb6
Luis Ceren <luiscerenc18@gmail.com>
2023-07-31 16:15:24 -0500

    Start: Function based vies for create retrive or list

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from products import views
 
 urlpatterns = [
-    path('', views.ProductCreateAPIView.as_view()),
+    # path('', views.ProductCreateAPIView.as_view()),
+    path('', views.ProductListCreateAPIView.as_view()),
     path('<int:pk>/', views.ProductDetailAPIView.as_view()),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -22,3 +22,16 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # lookup_field = 'pk'
+
+
+class ProductListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        print(serializer.validated_data)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
