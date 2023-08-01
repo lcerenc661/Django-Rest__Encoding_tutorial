e9aea8b7b274c6ff5d1343ce725bbc350d6f52f3
Luis Ceren <luiscerenc18@gmail.com>
2023-07-31 19:06:58 -0500

    UpdateApIView & DestroyAPIView

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,10 @@
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+# from django.http import Http404
+from django.shortcuts import get_object_or_404
+
 
 from products.models import Product
 from products.serializers import ProductSerializer
@@ -35,3 +41,31 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
         if content is None:
             content = title
         serializer.save(content=content)
+
+
+@api_view(['GET', 'POST'])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method
+
+    if method == "GET":
+        if pk is not None:
+            obj = get_object_or_404(Product, pk=pk)
+            print(obj)
+            data = ProductSerializer(obj, many=False).data
+            return Response(data)
+            # detail view
+
+        queryset = Product.objects.all()
+        data = ProductSerializer(queryset, many=True).data
+        return Response(data)
+
+    if method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content') or None
+            if content is None:
+                content = title
+            serializer.save(content=content)
+            return Response(serializer.data)
+        return Response({"invalid": "not good data"}, status=400)
--- py_client/not_found.py
+++ py_client/not_found.py
@@ -1,6 +1,6 @@
 import requests
 
-endpoint = "http://localhost:8000/api/products/6/"
+endpoint = "http://localhost:8000/api/products/656656/"
 
 get_response = requests.get(endpoint)
 
