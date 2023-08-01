f600b530b053a6b80df860d0e15a7ef16ec373f9
Luis Ceren <luiscerenc18@gmail.com>
2023-08-01 15:26:03 -0500

    model serializer create & update methods

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,14 +1,22 @@
 from rest_framework import serializers
-
+from rest_framework.reverse import reverse
 from products.models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
     discount = serializers.SerializerMethodField(read_only=True)
+    url = serializers.SerializerMethodField(read_only=True)
+    edit_url = serializers.SerializerMethodField(read_only=True)
+    other_url = serializers.HyperlinkedIdentityField(
+        view_name='product-detail',
+        lookup_field='pk')
 
     class Meta:
         model = Product
         fields = [
+            'url',
+            'edit_url',
+            'other_url',
             'pk',
             'title',
             'content',
@@ -16,6 +24,19 @@ class ProductSerializer(serializers.ModelSerializer):
             'sale_price',
             'discount',
         ]
+
+    def get_url(self, obj):
+        request = self.context.get('request')
+        if request is None:
+            return None
+        return reverse("product-detail", kwargs={"pk": obj.pk}, request=request)
+    
+    def get_edit_url(self, obj):
+        request = self.context.get('request')
+        if request is None:
+            return None
+        return reverse("product-edit", kwargs={"pk": obj.pk}, request=request)
+
 
     def get_discount(self, obj):
         if not hasattr(obj, 'id'):
--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,8 +3,12 @@ from products import views
 
 urlpatterns = [
     # path('', views.ProductCreateAPIView.as_view()),
-    path('', views.ProductListCreateAPIView.as_view()),
-    path('<int:pk>/', views.ProductDetailAPIView.as_view()),
-    path('<int:pk>/update/', views.ProductUpdateAPIView.as_view()),
-    path('<int:pk>/delete/', views.ProductDestroyAPIView.as_view()),
+    path('', views.ProductListCreateAPIView.as_view(),
+         name='product-list'),
+    path('<int:pk>/', views.ProductDetailAPIView.as_view(),
+         name='product-detail'),
+    path('<int:pk>/update/', views.ProductUpdateAPIView.as_view(),
+         name='product-edit'),
+    path('<int:pk>/delete/', views.ProductDestroyAPIView.as_view(),
+         name='product-delete'),
 ]
